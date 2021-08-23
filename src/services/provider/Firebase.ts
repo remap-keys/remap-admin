37fb40a1513cb22226c91416992a758aaa29d81b
Yoichiro Tanaka <yoichiro6642@gmail.com>
2021-08-23 17:15:20 +0900

    Fix the Firebase import statement warning.

--- src/services/provider/Firebase.ts
+++ src/services/provider/Firebase.ts
@@ -1,4 +1,7 @@
-import firebase from 'firebase';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import 'firebase/functions';
 import { IAuth } from '../auth/Auth';
 import {
   IFetchKeyboardDefinitionDetailResult,
